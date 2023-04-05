eb4f428408ce84987b366ccf923f6eacda269eca
py85252876 <pangyan13651155433@126.com>
2023-04-05 16:54:35 -0400

    third_version

--- gen_l2_gradients.py
+++ gen_l2_gradients.py
@@ -1,6 +1,5 @@
 import argparse
 import inspect
-import math
 import os
 from pathlib import Path
 from typing import Optional
@@ -11,24 +10,20 @@ import torch.nn.functional as F
 from accelerate import Accelerator
 from accelerate.logging import get_logger
 from datasets import load_dataset
-from diffusers import DDPMPipeline, DDPMScheduler, UNet2DModel
-from diffusers.optimization import get_scheduler
-from diffusers.training_utils import EMAModel
+from diffusers import DDPMScheduler, UNet2DModel
 from diffusers.utils import check_min_version
-from huggingface_hub import HfFolder, Repository, whoami
+from huggingface_hub import HfFolder, whoami
 from torchvision.transforms import (
     CenterCrop,
     Compose,
     InterpolationMode,
     Normalize,
-    RandomHorizontalFlip,
     Resize,
     ToTensor,
 )
 from tqdm.auto import tqdm
 
 
-# Will error if the minimal version of diffusers is not installed. Remove at your own risks.
 check_min_version("0.10.0.dev0")
 
 
@@ -36,15 +31,6 @@ logger = get_logger(__name__)
 
 
 def _extract_into_tensor(arr, timesteps, broadcast_shape):
-    """
-    Extract values from a 1-D numpy array for a batch of indices.
-
-    :param arr: the 1-D numpy array.
-    :param timesteps: a tensor of indices into the array to extract.
-    :param broadcast_shape: a larger shape of K dimensions with the batch
-                            dimension equal to the length of timesteps.
-    :return: a tensor of shape [batch_size, 1, ...] where the shape has K dims.
-    """
     if not isinstance(arr, torch.Tensor):
         arr = torch.from_numpy(arr)
     res = arr[timesteps].float().to(timesteps.device)
@@ -54,44 +40,28 @@ def _extract_into_tensor(arr, timesteps, broadcast_shape):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Simple example of a training script.")
+    parser = argparse.ArgumentParser(description="set dataset name")
     parser.add_argument(
         "--dataset_name",
         type=str,
         default=None,
         help=(
-            "The name of the Dataset (from the HuggingFace hub) to train on (could be your own, possibly private,"
-            " dataset). It can also be a path pointing to a local copy of a dataset in your filesystem,"
-            " or to a folder containing files that HF Datasets can understand."
+            "The name of the Dataset to train."
         ),
-    )
-    parser.add_argument(
-        "--dataset_config_name",
-        type=str,
-        default=None,
-        help="The config of the Dataset, leave as None if there's only one config.",
     )
     parser.add_argument(
         "--train_data_dir",
         type=str,
         default=None,
         help=(
-            "A folder containing the training data. Folder contents must follow the structure described in"
-            " https://huggingface.co/docs/datasets/image_dataset#imagefolder. In particular, a `metadata.jsonl` file"
-            " must exist to provide the captions for the images. Ignored if `dataset_name` is specified."
+            "training dataset directory"
         ),
     )
     parser.add_argument(
         "--model_dir",
         type=str,
-        default="ddpm-model-64",
-        help="The output directory where the model predictions and checkpoints will be written.",
-    )
-    parser.add_argument(
-        "--cache_dir",
-        type=str,
         default=None,
-        help="The directory where the downloaded models and datasets will be stored.",
+        help="Where we save the training and shadow model",
     )
     parser.add_argument(
         "--resolution",
@@ -102,77 +72,22 @@ def parse_args():
             " resolution"
         ),
     )
-
-    parser.add_argument(
-        "--dataloader_num_workers",
-        type=int,
-        default=0,
-        help=(
-            "The number of subprocesses to use for data loading. 0 means that the data will be loaded in the main"
-            " process."
-        ),
-    )
-    parser.add_argument(
-        "--adam_weight_decay", type=float, default=1e-6, help="Weight decay magnitude for the Adam optimizer."
-    )
-    parser.add_argument("--adam_epsilon", type=float, default=1e-08, help="Epsilon value for the Adam optimizer.")
-    parser.add_argument(
-        "--use_ema",
-        action="store_true",
-        default=True,
-        help="Whether to use Exponential Moving Average for the final model weights.",
-    )
-    parser.add_argument("--ema_inv_gamma", type=float, default=1.0, help="The inverse gamma value for the EMA decay.")
-    parser.add_argument("--ema_power", type=float, default=3 / 4, help="The power value for the EMA decay.")
-    parser.add_argument("--ema_max_decay", type=float, default=0.9999, help="The maximum decay magnitude for EMA.")
-
-    parser.add_argument(
-        "--logging_dir",
-        type=str,
-        default="logs",
-        help=(
-            "[TensorBoard](https://www.tensorflow.org/tensorboard) log directory. Will default to"
-            " *output_dir/runs/**CURRENT_DATETIME_HOSTNAME***."
-        ),
-    )
     parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
     parser.add_argument("--which_l2", type=int, default=-1, help="-1 is the last checkpoint")
-    parser.add_argument(
-        "--mixed_precision",
-        type=str,
-        default="no",
-        choices=["no", "fp16", "bf16"],
-        help=(
-            "Whether to use mixed precision. Choose"
-            "between fp16 and bf16 (bfloat16). Bf16 requires PyTorch >= 1.10."
-            "and an Nvidia Ampere GPU."
-        ),
-    )
     parser.add_argument(
         "--prediction_type",
         type=str,
         default="epsilon",
         choices=["epsilon", "sample"],
-        help="Whether the model should predict the 'epsilon'/noise error or directly the reconstructed image 'x0'.",
+        help="Whether the model predict the 'epsilon'",
     )
     parser.add_argument("--ddpm_num_steps", type=int, default=1000)
-    parser.add_argument("--ddpm_beta_schedule", type=str, default="linear")
-    parser.add_argument(
-        "--checkpointing_steps",
-        type=int,
-        default=5000,
-        help=(
-            "Save a checkpoint of the training state every X updates. These checkpoints are only suitable for resuming"
-            " training using `--resume_from_checkpoint`."
-        ),
-    )
     parser.add_argument(
         "--resume_from_checkpoint",
         type=str,
         default=None,
         help=(
-            "Whether training should be resumed from a previous checkpoint. Use a path saved by"
-            ' `--checkpointing_steps`, or `"latest"` to automatically select the last available checkpoint.'
+            "whether we will get the latest checkpoints"
         ),
     )
     parser.add_argument(
@@ -180,8 +95,7 @@ def parse_args():
         type=str,
         default=None,
         help=(
-            "Whether training should be resumed from a previous checkpoint. Use a path saved by"
-            ' `--checkpointing_steps`, or `"latest"` to automatically select the last available checkpoint.'
+            "The directory that save the gradient information."
         ),
     )
 
@@ -190,36 +104,8 @@ def parse_args():
     if env_local_rank != -1 and env_local_rank != args.local_rank:
         args.local_rank = env_local_rank
 
-    if args.dataset_name is None and args.train_data_dir is None:
-        raise ValueError("You must specify either a dataset name from the hub or a train data directory.")
-
     return args
 
-
-def get_full_repo_name(model_id: str, organization: Optional[str] = None, token: Optional[str] = None):
-    if token is None:
-        token = HfFolder.get_token()
-    if organization is None:
-        username = whoami(token)["name"]
-        return f"{username}/{model_id}"
-    else:
-        return f"{organization}/{model_id}"
-from PIL import Image
-
-def numpy_to_pil(images):
-        """
-        Convert a numpy image or a batch of images to a PIL image.
-        """
-        if images.ndim == 3:
-            images = images[None, ...]
-        images = (images * 255).round().astype("uint8")
-        if images.shape[-1] == 1:
-            # special case for grayscale (single channel) images
-            pil_images = [Image.fromarray(image.squeeze(), mode="L") for image in images]
-        else:
-            pil_images = [Image.fromarray(image) for image in images]
-
-        return pil_images
 def main(args):
     accelerator = Accelerator()
 
@@ -251,11 +137,11 @@ def main(args):
     if accepts_prediction_type:
         noise_scheduler = DDPMScheduler(
             num_train_timesteps=args.ddpm_num_steps,
-            beta_schedule=args.ddpm_beta_schedule,
+            beta_schedule='linear',
             prediction_type=args.prediction_type,
         )
     else:
-        noise_scheduler = DDPMScheduler(num_train_timesteps=args.ddpm_num_steps, beta_schedule=args.ddpm_beta_schedule)
+        noise_scheduler = DDPMScheduler(num_train_timesteps=args.ddpm_num_steps, beta_schedule='linear')
 
     optimizer = torch.optim.AdamW(
         model.parameters(),
@@ -266,7 +152,6 @@ def main(args):
         [
             Resize(args.resolution, interpolation=InterpolationMode.BILINEAR),
             CenterCrop(args.resolution),
-            # RandomHorizontalFlip(),
             ToTensor(),
             Normalize([0.5], [0.5]),
         ]
@@ -274,12 +159,12 @@ def main(args):
     if args.dataset_name is not None:
         dataset = load_dataset(
             args.dataset_name,
-            args.dataset_config_name,
-            cache_dir=args.cache_dir,
+            None,
+            cache_dir=None,
             split="train",
         )
     else:
-        dataset = load_dataset("imagefolder", data_dir=args.train_data_dir, cache_dir=args.cache_dir, split="train")
+        dataset = load_dataset("imagefolder", data_dir=args.train_data_dir, cache_dir=None, split="train")
 
     def transforms(examples):
         images = [augmentations(image.convert("RGB")) for image in examples["image"]]
@@ -287,7 +172,7 @@ def main(args):
 
     dataset.set_transform(transforms)
     train_dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, shuffle=True, num_workers=args.dataloader_num_workers
+        dataset, batch_size=1, shuffle=True, num_workers=0
     )
 
     model, optimizer, train_dataloader = accelerator.prepare(
@@ -305,7 +190,6 @@ def main(args):
             path = os.path.basename(args.resume_from_checkpoint)
             print(path)
         else:
-            # Get the most recent checkpoint
             dirs = os.listdir(args.model_dir)
             dirs = [d for d in dirs if d.startswith("checkpoint")]
             dirs = sorted(dirs, key=lambda x: int(x.split("-")[1]))
@@ -313,7 +197,6 @@ def main(args):
             print(path)
         accelerator.print(f"Resuming from checkpoint {path}")
         accelerator.load_state(os.path.join(args.model_dir, path))
-        global_step = int(path.split("-")[1])
 
 
     for epoch in range(0, 1):
@@ -333,7 +216,6 @@ def main(args):
             #change timesteps from 1 to 1499.
             timesteps = torch.tensor(timesteps, device = clean_images.device).long()
             # Add noise to the clean images according to the noise magnitude at each timestep
-            # (this is the forward diffusion process)
             noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
 
             with accelerator.accumulate(model):
@@ -341,7 +223,7 @@ def main(args):
                 model_output = model(noisy_images, timesteps).sample
 
                 if args.prediction_type == "epsilon":
-                    loss = F.mse_loss(model_output, noise)  # this could have different weights!
+                    loss = F.mse_loss(model_output, noise)  # this could have different weights
                 elif args.prediction_type == "sample":
                     alpha_t = _extract_into_tensor(
                         noise_scheduler.alphas_cumprod, timesteps, (clean_images.shape[0], 1, 1, 1)
